4524aee2b9372f2b9667b89dc0f946f68eb62943
Pedro Quadro Machado <86629592+Pedro-xexa@users.noreply.github.com>
2022-05-11 18:10:46 -0300

    Prova Turma B - Questão 1
    pontos)Um barman inventou um novo drink que é composto por três ingredientes líquidos. Faça
    um programa que leia as quantidades utilizadas, em ml (valores reais), de cada um dos três ingredientes, calcule e
    exiba o percentual de cada ingrediente na composição do novo drink. Obrigatoriamente, o seu programa deverá
    utilizar a função abaixo, desenvolvida por você:
    a. percentual: recebe como parâmetros as quantidades utilizadas de cada um dos três ingredientes, calcula e
    retorna o percentual do primeiro ingrediente recebido como parâmetro na composição do drink;

--- main.c
+++ main.c
@@ -1,94 +1,22 @@
 #include <stdio.h>
 
-void fexibe(float fvalotsemdesconto, float fdescontoporvalor, float fdescontopormes, float fvalorfinalapagar){
-  printf("\n-------------------------------");
-  printf("\nValor total da encomenda: %.2fR$",fvalotsemdesconto);
-  printf("\nDesconto por valor total: %.2fR$",fdescontoporvalor);
-  printf("\nDesconto por mês da compra: %.2fR$",fdescontopormes);
-  printf("\nValor final a pagar: %.2fR$",fvalorfinalapagar);
-  printf("\n-------------------------------\n");
-}
-
-
-int descontopormes(int fmes, int fvalorcomdesconto){
-  int qtd, descontomesfinal, variavel;
-  qtd = fvalorcomdesconto/500;
-
-  if(fmes==11 || fmes==12){
-    variavel=20;
-  }
-  else{
-    if(fmes==8){
-      variavel=80;
-    }
-    else{
-      variavel=35;
-    }
-  }
-
-  descontomesfinal= qtd*variavel;
-  return descontomesfinal;
-}
-
-
-float fdescontoporvalortotal(int fvalortotal){
-  float percent;
-  if(fvalortotal<=999.99){
-    percent=0;
-  }
-  else{
-    if(fvalortotal<=2999.99){
-      percent=0.25;
-    }
-    else{
-      if(fvalortotal<=4999.99){
-        percent=0.35;
-      }
-      else{
-        percent=0.4;
-      }
-    }
-  }
-  
-  return percent*fvalortotal;
-}
-
-
-int funcaocodigo(int fcodigo){
-  int preco;
-  if (fcodigo%2==0){
-    preco=10;
-  }
-  else{
-    preco=20;
-  }
-  return preco;
+float calculopercent (float fing1, float fing2, float fing3){
+  return fing1*100/(fing1+fing2+fing3);
 }
 
 
 int main(void) {
-  int codigo, quantidade, mes, valorcom1desconto;
-  float valortotal, descontomes, descontoporvalortotal, valorapagar;
-  printf("Qual o codigo do produto: ");
-  scanf("%d",&codigo);
-  printf("\nDigite a quantidade comprada: ");
-  scanf("%d",&quantidade);
-  printf("\nDigite o mês da compra: ");
-  scanf("%d",&mes);
+  int ingrediente1, ingrediente2, ingrediente3;
+  printf("Digite a quantidade do 1° ingrediente: ");
+  scanf("%d",&ingrediente1);
+  printf("\nDigite a quantidade do 2° ingrediente: ");
+  scanf("%d",&ingrediente2);
+  printf("\nDigite a quantidade do 3° ingrediente: ");
+  scanf("%d",&ingrediente3);
+
   
-  valortotal=funcaocodigo(codigo)*quantidade;
-  descontoporvalortotal = fdescontoporvalortotal(valortotal);
-  valorcom1desconto=valortotal-descontoporvalortotal;
-  descontomes=descontopormes(mes,valorcom1desconto);
-  valorapagar=valorcom1desconto-descontomes;
-
-  fexibe(valortotal, descontoporvalortotal, descontomes, valorapagar);
-
-
+  printf("\nO percentual do primeiro ingrediente é %.2f%%", calculopercent(ingrediente1, ingrediente2, ingrediente3));
+  printf("\nO percentual do segundo ingrediente é %.2f%%", calculopercent(ingrediente2, ingrediente1, ingrediente3));
+  printf("\nO percentual do terceiro ingrediente é %.2f%%", calculopercent(ingrediente3, ingrediente2, ingrediente1));
   return 0;
 }
-
-//mostrar valor total da encomenda sem desconto
-// mostrar o desconto pelo valor total
-//Mostrar desconto pelo mês
-//mostrar o valor final a pagar
