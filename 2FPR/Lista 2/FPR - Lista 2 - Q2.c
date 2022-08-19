808d222debdd2967b8d38a235d347816fa306e7d
Pedro Quadro Machado <86629592+Pedro-xexa@users.noreply.github.com>
2022-08-19 12:46:15 -0300

    Update and rename FPR - Lista 2 - Q1.c to FPR - Lista 2 - Q2.c

--- 2FPR/Lista 2/FPR - Lista 2 - Q2.c
+++ 2FPR/Lista 2/FPR - Lista 2 - Q2.c
@@ -1,3 +1,5 @@
+/*Implementar uma função que, dado um vetor de reais, troque o 1° e o 2° elementos, 
+em seguida o 3° e o 4° elementos e assim sucessivamente, até se chegar ao final do vetor.*/
 #include <stdio.h>
 
 void troca (int v[], int tam){
